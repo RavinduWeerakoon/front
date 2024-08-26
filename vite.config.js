0ee2e7e727d66084831f2541e3339164f1dc1868
Ravindu Weerakoon <ravinduhiran26@gmail.com>
2024-08-27 01:21:37 +0530

    fixing the error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: [
       {
@@ -16,4 +18,5 @@ export default defineConfig({
       },
     ],
   },
+
 })
