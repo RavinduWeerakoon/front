07c297a828ae581b08393ae9084c88c7482cc7a8
OmalyaV <119015789+OmalyaV@users.noreply.github.com>
2024-08-30 19:49:37 +0530

    used redux persist to make the context permenant

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import store from './store/store.js'
+import {store,persistor} from './store/store.js'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <PersistGate persistor={persistor}>
   <Provider store={store}>
   
     <App />
 
   </Provider>
+  </PersistGate>
   </StrictMode>
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,26 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import authReducer from './authSlice';
-export default configureStore({
-    reducer: {
-        auth: authReducer
-    }
+
+import storage from 'redux-persist/lib/storage';
+import { persistStore } from 'redux-persist';
+import { persistReducer } from 'redux-persist';
+const rootReducer = combineReducers({
+    auth: authReducer
 });
+const persistConfig = {
+    key: "root",
+    storage,
+    version: 1
+};
+
+const persistedState = persistReducer(persistConfig, rootReducer);
+export const store= configureStore({
+    
+        reducer: persistedState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+            serializableCheck: false
+        })
+    
+});
+
+export const persistor = persistStore(store);
