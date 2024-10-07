d2cce3336c4a832e1b4cad425d3123c5710d1475
Ravindu Weerakoon <81863859+RavinduWeerakoon@users.noreply.github.com>
2024-10-07 12:37:53 +0000

    fixing the profile page

--- src/Dashboard/Components/ProfileDetails.jsx
+++ src/Dashboard/Components/ProfileDetails.jsx
@@ -15,17 +15,17 @@ import EmotionScore from './Charts/EmotionScore';
 import StreakIcon from './Charts/StreakIcon';
 import StatCard from './Charts/StatCard';
 
-const data = 
-  {
-    title: 'Users',
-    value: '14k',
-    interval: 'Last 30 days',
-    trend: 'up',
-    data: [
-      200, 24, 220, 260, 240, 380, 100, 240, 280, 240, 300, 340, 320, 360, 340, 380,
-      360, 400, 380, 420, 400, 640, 340, 460, 440, 480, 460, 600, 880, 920,
-    ],
-  }
+const data =
+{
+  title: 'Users',
+  value: '14k',
+  interval: 'Last 30 days',
+  trend: 'up',
+  data: [
+    200, 24, 220, 260, 240, 380, 100, 240, 280, 240, 300, 340, 320, 360, 340, 380,
+    360, 400, 380, 420, 400, 640, 340, 460, 440, 480, 460, 600, 880, 920,
+  ],
+}
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -42,82 +42,80 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function FullWidthGrid() {
   return (
 
-    <Box sx={{ flexGrow: 1 ,m:2 }}>
+    <Box sx={{ flexGrow: 1, m: 2 }}>
       <Grid container spacing={2}>
-        <Grid item xs={6} md={8}>
-        <Stack spacing={2} direction="row">
-        <Card sx={{maxWidth: 40+"%", minWidth:30+"%"}}>
-          <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <Grid item xs={8} md={4}>
+          <Card sx={{justifyContent:'center', alignItems:"center", marginLeft:"auto", marginRight:"auto"}}>
+            <CardContent justifyContent="center" allignItems="center" >
+              <Typography gutterBottom variant="h5" component="div">
 
-          Mood Insights
-          </Typography>
+                This Weeks Score
+              </Typography>
 
-          <EmotionScore score={80} />
-        </CardContent>
-      </Card>
+              <EmotionScore score={80} />
+            </CardContent>
+          </Card>
 
-      <Card sx={{ maxWidth: 40+"%", minWidth:30+"%"}}>
+        </Grid>
+        <Grid item xs={4} md={4}>
 
-          <CardContent>
-        <StatCard {...data}/>
-        </CardContent>
-      </Card>
-      </Stack>
+              <StatCard {...data} />
+         
+
 
 
         </Grid>
         <Grid item xs={6} md={4}>
-            <Stack spacing={2}>
-              
-              <Card sx={{maxWidth: 100+"%", height:50+"%" }}>
-              <CardContent>
-              <StreakIcon streakCount={20}/>
-              </CardContent>
-              </Card>
+          <Stack spacing={2}>
 
-              <Card sx={{ maxWidth: 100+"%", height:50+"%" }}>
+            <Card sx={{ maxWidth: 100 + "%", height: 50 + "%" }}>
               <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-              Emotion Pie Chart
-              </Typography>
+                <StreakIcon streakCount={20} />
               </CardContent>
-              </Card>
-              
-              
-        
+            </Card>
+
+            <Card sx={{ maxWidth: 100 + "%", height: 50 + "%" }}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Emotion Pie Chart
+                </Typography>
+              </CardContent>
+            </Card>
+
+
+
           </Stack>
         </Grid>
 
       </Grid>
 
-      <Box height={20} sx={{marginTop:3,}}>
-      
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={8}>
-        <Card sx={{height:60+"vh"}}>
-        <CardContent sx={{height:"100%"}}>
-          <WeeklyEmotions />
-        </CardContent>
-        </Card>
+      <Box height={20} sx={{ marginTop: 3, }}>
+
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}>
+            <Card sx={{ height: 60 + "vh" }}>
+              <CardContent sx={{ height: "100%" }}>
+                <WeeklyEmotions />
+              </CardContent>
+            </Card>
+
+          </Grid>
+          <Grid item xs={6} md={4}>
+
+            <Card sx={{ height: 60 + "vh" }}>
+              <CardContent sx={{ height: "100%" }}>
+                <EmototionPieChart />
+              </CardContent>
+            </Card>
+          </Grid>
 
         </Grid>
-        <Grid item xs={6} md={4}>
-
-        <Card sx={{height:60+"vh"}}>
-        <CardContent sx={{height:"100%"}}>
-          <EmototionPieChart />
-        </CardContent>
-        </Card>
-        </Grid>
-
-      </Grid>
 
       </Box>
 
-      
-      
+
+
     </Box>
-    
+
   );
 }
