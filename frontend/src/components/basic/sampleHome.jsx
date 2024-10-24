cc320d815f4a8b82d113876dc2cdf14ea8365a88
Ravindu <ravinduhiran26@gmail.com>
2024-10-24 19:54:41 +0530

    changes to the frontend

--- frontend/src/components/basic/sampleHome.jsx
+++ frontend/src/components/basic/sampleHome.jsx
@@ -4,40 +4,17 @@ import Typography from '@mui/material/Typography';
 import CssBaseline from '@mui/material/CssBaseline';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Grid  from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import {Link} from 'react-router-dom';
 import { getJournals } from '../../services/journalService';
 import { useSelector } from 'react-redux';
 import { useTheme } from '@mui/material/styles';
-
+import FormDialog from './FormDialog';
 import JournalEntry from './ListEntry';
 
 
 function NewButton() {
   return (
     
-    <Button
-      component={Link}
-      to="dashboard/new"
-      variant="contained"
-      startIcon={<AddIcon />}
-      sx={{
-        backgroundColor: '#A5E5FF',
-        marginTop: '2rem',
-        borderRadius: '30px',
-        color: '#083347',
-        textTransform: 'none',
-        fontWeight: 'bold',
-        boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.16)',
-        padding: '10px 20px',
-        '&:hover': {
-          backgroundColor: '#90d4f7',
-        },
-      }}
-    >
-      New
-    </Button>
+  <FormDialog/>
   );
 }
 
@@ -49,13 +26,11 @@ function SampleHome() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const {uid} = useSelector((state) => state.auth);
-  console.log(uid)
   
 useEffect(()=> {
   const fetchJournals =async ()=>{
     try{
     const journalEntries = await getJournals(uid);
-    console.log(journalEntries)
     setJournals(journalEntries)
     }
     catch(error){
@@ -69,11 +44,13 @@ useEffect(()=> {
     <Container maxWidth="md" sx={{p:2}}>
     <Grid justifyContent="left" alignItems="left" maxWidth="md">
       <CssBaseline />
-
+      <Grid container justifyContent="right" alignItems="end"><NewButton/></Grid>
 
 <Typography variant="h3" component="h2" style={{ textAlign: 'left', marginBottom:3 }}>
   Stories
 </Typography>
+
+
       
 {error ? (
           <Typography variant="h6" color="error">
@@ -90,7 +67,6 @@ useEffect(()=> {
         )}
     </Grid>
 
-    <Grid container justifyContent="right" alignItems="end"><NewButton/></Grid>
 </Container>
 
   );
